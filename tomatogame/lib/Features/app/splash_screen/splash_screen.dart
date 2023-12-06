171cdab2d44fbcfa88bb5511eaffad0c08a6076a
Bikash <bikashchaudhary007@gmail.com>
2023-12-06 11:41:48 +0545

    Added animation in Splash Screen

--- tomatogame/lib/Features/app/splash_screen/splash_screen.dart
+++ tomatogame/lib/Features/app/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 import '../../user_auth/presentation/pages/login_page.dart';
 
@@ -15,26 +16,27 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     // For 3 second delayed
-    Future.delayed(
-      Duration(seconds: 3),
-        (){
-        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>widget.child!), (route) => false);
-        }
-    );
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => widget.child!),
+          (route) => false);
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text(
-        "Welcome to Tomato Game",
-        style: TextStyle(
-          color: Colors.redAccent,
-          fontWeight: FontWeight.bold,
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Lottie.network(
+              'https://lottie.host/a8404503-3401-41ab-a2fa-a2f4abaff61a/oAv5Rx7hFD.json'),
         ),
-      ),
+
+      ],
     ));
   }
 }
